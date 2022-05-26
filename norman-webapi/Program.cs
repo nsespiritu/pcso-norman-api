1f1ce4483edd8087e4943c428d635cb78de0e5ea
nsespiritu@yahoo.com <nsespiritu@yahoo.com>
2022-05-26 10:54:58 +0800

    update the program with functionality of chat for signal R

--- norman-webapi/Program.cs
+++ norman-webapi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,66 +8,80 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSignalR();
+
+builder.Services.AddCors();
+
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
+app.MapHub<Chat>(nameof(Chat));
+
 app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/todoitems", async (TodoDb db) =>
-    await db.Todos.ToListAsync());
+	await db.Todos.ToListAsync());
 
 app.MapGet("/todoitems/complete", async (TodoDb db) =>
-    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+	await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
 app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
-    await db.Todos.FindAsync(id)
-        is Todo todo
-            ? Results.Ok(todo)
-            : Results.NotFound());
+	await db.Todos.FindAsync(id)
+		is Todo todo
+			? Results.Ok(todo)
+			: Results.NotFound());
 
 app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
 {
-    db.Todos.Add(todo);
-    await db.SaveChangesAsync();
+	db.Todos.Add(todo);
+	await db.SaveChangesAsync();
 
-    return Results.Created($"/todoitems/{todo.Id}", todo);
+	return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
 app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
 {
-    var todo = await db.Todos.FindAsync(id);
+	var todo = await db.Todos.FindAsync(id);
 
-    if (todo is null) return Results.NotFound();
+	if (todo is null) return Results.NotFound();
 
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
+	todo.Name = inputTodo.Name;
+	todo.IsComplete = inputTodo.IsComplete;
 
-    await db.SaveChangesAsync();
+	await db.SaveChangesAsync();
 
-    return Results.NoContent();
+	return Results.NoContent();
 });
 
 app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
 {
-    if (await db.Todos.FindAsync(id) is Todo todo)
-    {
-        db.Todos.Remove(todo);
-        await db.SaveChangesAsync();
-        return Results.Ok(todo);
-    }
+	if (await db.Todos.FindAsync(id) is Todo todo)
+	{
+		db.Todos.Remove(todo);
+		await db.SaveChangesAsync();
+		return Results.Ok(todo);
+	}
 
-    return Results.NotFound();
+	return Results.NotFound();
 });
 
 // new code
 app.MapDelete("/todoitems", async (TodoDb db) =>
 {
-    await db.Database.EnsureDeletedAsync();
-    await db.SaveChangesAsync();
-    return Results.Ok(null);
+	await db.Database.EnsureDeletedAsync();
+	await db.SaveChangesAsync();
+	return Results.Ok(null);
 });
+
+app.UseCors(builder => builder
+		 .AllowAnyHeader()
+		 .AllowAnyMethod()
+		 .SetIsOriginAllowed((host) => true)
+		 .AllowCredentials()
+	 );
+
 
 /*
 app.MapDelete("/todoitems", async (TodoDb db) =>
@@ -87,20 +102,28 @@ app.MapDelete("/todoitems", async (TodoDb db) =>
 
 app.Run();
 
+public class Chat : Hub
+{
+	public void Broadcast(string name, string message)
+	{
+		Clients.All.SendAsync("Receive", name, message);
+	}
+}
+
 class Todo
 {
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsComplete { get; set; }
-    public string? Message { get; set; }
-    public DateTime? Created { get; set; } = DateTime.Now;
+	public int Id { get; set; }
+	public string? Name { get; set; }
+	public bool IsComplete { get; set; }
+	public string? Message { get; set; }
+	public DateTime? Created { get; set; } = DateTime.Now;
 }
 
 class TodoDb : DbContext
 {
-    public TodoDb(DbContextOptions<TodoDb> options)
-        : base(options) { }
+	public TodoDb(DbContextOptions<TodoDb> options)
+		: base(options) { }
 
-    public DbSet<Todo> Todos => Set<Todo>();
+	public DbSet<Todo> Todos => Set<Todo>();
 }
 
